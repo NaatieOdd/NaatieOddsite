7f7769438d9f0d283a5ca0ea5f1dd851317b64e1
Nathan <Naatie.Odd@gmail.com>
2024-08-22 22:25:24 +0200

    bug fix
    Signed-off-by: Nathan <Naatie.Odd@gmail.com>

--- resources/views/home/layout/layout.blade.php
+++ resources/views/home/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta name="description" content="Nathan Oddings portfolio">
+    <meta name="description" content="Webstuff made by NaatieOdd.">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- resources/views/schematics/show.blade.php
+++ resources/views/schematics/show.blade.php
@@ -8,7 +8,7 @@
     <div class="schematic">
         <label for="title"> Title: {{ $schematic->title}}</label> <br>
         <label for="description">Description: {{ $schematic->description}}</label> <br>
-        <div><label for="creator">Creator: {{$schematic->creator}}</label></div>
+        <div><label for="creator">Creator:{{ $schematic->user->name }}</label></div>
         <button onclick="window.location='{{ route('schematics.download', ['id' => $schematic->id]) }}'">Download
             Schematic
         </button>
